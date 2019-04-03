0698d60403d44295a4030293c7dbb83bf29f1029
Caio Vinicius Carvalho de Cerqueira <caio_vinicius89@hotmail.com>
2019-04-03 20:40:58 -0300

    initial commit

---
+++ MultipleHttpWebRequestsWithParallelForeach/Program.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleHttpWebRequestsWithParallelForeach
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("* Múltiplas requisições HTTP com Parallel.Foreach *");
+            Console.WriteLine("\nO processo iniciou...");
+
+            var cepsList = GetCepList(); // pego a lista de ceps através de algum input, tipo: texto, banco de dados e etc...
+
+            var addressList = new List<Address>();
+
+            var stopWatch = new Stopwatch(); //Fornece métodos para medir o tempo.
+
+            stopWatch.Start();
+
+            Parallel.ForEach(cepsList, (cep) =>
+            {
+                var Url = $"https://viacep.com.br/ws/{cep}/json/";
+
+                try // importante colocar um tratamento de exceção para não quebrar toda a iteração caso hava algum erro/exceção
+                {
+                    string streamResponseAsJson = GetJsonResponseFromWebService(Url);
+
+                    if (string.IsNullOrEmpty(streamResponseAsJson))
+                        return;
+
+                    var address = JsonConvert.DeserializeObject<Address>(streamResponseAsJson);
+
+                    lock (addressList) // equanto a thread atual está incrementando este recuso compartilhado, nenhuma outra thread poderá fazer está operação.
+                    {
+                        addressList.Add(address);
+                    }
+                }
+                catch
+                {
+                    return; // continua a iteração, funciona como o continue do for/ foreach
+                }
+            });
+
+            stopWatch.Stop();
+
+            Console.WriteLine($"\nTempo de execução: {stopWatch.Elapsed}");
+            Console.WriteLine($"Quantidade de endereços importados: {addressList.Count()}");
+
+            //após as iterações paralelas, faça o que quiser com a lista...
+        }
+
+        private static string GetJsonResponseFromWebService(string Url)
+        {
+            string streamResponseAsJson = "";
+
+            var request = (HttpWebRequest)WebRequest.Create(Url);
+
+            request.ContentType = "application/json; charset=utf-8";
+
+            var response = request.GetResponse() as HttpWebResponse;  // faço uma requisição ao webservice da Viacep
+
+            using (var responseStream = response.GetResponseStream())
+            {
+                var reader = new StreamReader(responseStream, Encoding.UTF8);
+
+                streamResponseAsJson = reader.ReadToEnd();
+            }
+
+            return streamResponseAsJson;
+        }
+
+        private static List<string> GetCepList()
+        {
+            var cepsFile = File.ReadAllLines("Ceps.txt");
+
+            return new List<string>(cepsFile);
+        }
+    }
+}
